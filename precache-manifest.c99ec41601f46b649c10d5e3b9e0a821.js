01f48de47fbd9d173506183b2ec0a6cfafdd3d48
= <=>
2020-08-29 17:16:08 +0530

    new template

---
+++ precache-manifest.c99ec41601f46b649c10d5e3b9e0a821.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7b5c691f7f287d423560667d3f56dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568d24e2ea053d704d42",
+    "url": "/static/css/main.4e1d7590.chunk.css"
+  },
+  {
+    "revision": "a057ef0a4ef3d10519d0",
+    "url": "/static/js/2.1a45158c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a45158c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568d24e2ea053d704d42",
+    "url": "/static/js/main.2c98ae17.chunk.js"
+  },
+  {
+    "revision": "a0cfefc36c75b62f3ce3",
+    "url": "/static/js/runtime-main.b992024f.js"
+  },
+  {
+    "revision": "e3e1b60a55bd857c30d3d29e5eeba9c1",
+    "url": "/static/media/bg.e3e1b60a.jpg"
+  },
+  {
+    "revision": "88d7a64e46e863173d6ffb23d7145ec8",
+    "url": "/static/media/naren.88d7a64e.png"
+  }
+]);
